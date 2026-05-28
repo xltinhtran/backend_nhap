56b00200db065352ad3225b384ae14a58ea56910
xltinhtran <trantinh.674424@gmail.com>
2026-05-28 16:22:01 +0700

    api đăng nhập đăng ký

---
+++ BackendQuizletclone/Controllers/ActivityLogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendQuizletclone.Controllers
+{
+    public class ActivityLogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
